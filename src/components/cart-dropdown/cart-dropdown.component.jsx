977fee825171e11c1b4a0f353924754b943d0348
Ngân Nguyễn <>
2023-03-17 22:05:52 +0900

    Total-price

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../contexts/cart.context";
 
 import "./cart-dropdown.styles.scss";
@@ -9,6 +11,11 @@ import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
 
   return (
     <div className="cart-dropdown-container">
@@ -21,7 +28,7 @@ const CartDropdown = () => {
           <span className="empty-message"></span>
         )}
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
